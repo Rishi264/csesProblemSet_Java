cab6bd0842c77d39708276611568d1e3468e49b4
Rishikesh M Patidar <71841477+Rishi264@users.noreply.github.com>
2023-02-14 15:25:47 +0530

    Add files via upload

---
+++ WierdAlgorithm.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+class WierdAlgorithm{
+    public static void main(String args[])
+    {
+        Scanner scanner=new Scanner(System.in);
+        long n=scanner.nextLong();
+        System.out.print(n+" ");
+        while(n!=1)
+        {
+            if(n%2==0)
+            {
+                n=n/2;
+            }
+            else{
+                n=(n*3)+1;
+            }
+            System.out.print(n+" ");
+        }
+    }
+}
